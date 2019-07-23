2215e1e042fcce4527c62cf9b3bee953809082b9
ashishg1 <ashishg1@gmail.com>
2019-07-23 18:18:59 -0400

    add basic tests

---
+++ src/main/java/com/loyalty/homework/controllers/RestApi.java
@@ -0,0 +1,14 @@
+package com.loyalty.homework.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApi {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Hello world";
+    }
+}
