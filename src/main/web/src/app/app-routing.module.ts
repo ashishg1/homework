c24ecd8db7bd77747dbe578d75934f0dfeba0a00
ashishg1 <ashishg1@gmail.com>
2019-07-24 12:41:48 -0400

    Step 3: Simple angular

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { MessageComponent } from './message/message.component';
+
+const routes: Routes = [
+{ path: 'message', component: MessageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
