2f70326a256d71bb7d6aac6754925101d3f09b0d
ashishg1 <ashishg1@gmail.com>
2019-07-26 21:25:00 -0400

    Remove branch info

--- cloudformation/deployEnv.bat
+++ cloudformation/deployEnv.bat
@@ -1,6 +1,6 @@
 set projectName=%1
 aws cloudformation deploy --template codebuild.yml --stack-name %projectName%codebuild --capabilities CAPABILITY_IAM --parameter-overrides ProjectName=%projectName%
-aws codebuild start-build --project-name %projectName% --source-version docker
+aws codebuild start-build --project-name %projectName%
 aws s3api wait object-exists --bucket %projectName%bucketcodedeploy --key app.zip
 aws cloudformation deploy --template ebs.yml --stack-name %projectName%ebs --parameter-overrides ProjectName=%projectName%
 aws cloudformation list-exports
