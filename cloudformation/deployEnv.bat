cb053ca12ae42407933cb36d0432412f87e02e0f
ashishg1 <ashishg1@gmail.com>
2019-07-26 23:54:10 -0400

    fixes

--- cloudformation/deployEnv.bat
+++ cloudformation/deployEnv.bat
@@ -2,5 +2,7 @@ set projectName=%1
 aws cloudformation deploy --template codebuild.yml --stack-name %projectName%codebuild --capabilities CAPABILITY_IAM --parameter-overrides ProjectName=%projectName%
 aws codebuild start-build --project-name %projectName%
 aws s3api wait object-exists --bucket %projectName%bucketcodedeploy --key app.zip
-aws cloudformation deploy --template ebs.yml --stack-name %projectName%ebs --parameter-overrides ProjectName=%projectName%
+aws s3api wait object-exists --bucket %projectName%bucketcodedeploy --key app.zip
+aws s3api wait object-exists --bucket %projectName%bucketcodedeploy --key app.zip
+aws cloudformation deploy --template ebs.yml --stack-name %projectName%ebs --parameter-overrides ProjectName=%projectName% --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 aws cloudformation list-exports
